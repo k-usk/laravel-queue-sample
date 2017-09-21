7c7aa630f6a35e49037fea37d9056b86754e2155
Yusuke Kano <kano@tam-tam.co.jp>
2017-09-21 14:45:30 +0900

    fix ide

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+//        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
